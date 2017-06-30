21058bdac48ff3357df95c9aff162e88933142dc
OFFICE\hkaraagac <hakan.karaagac@netnames.com>
2017-06-30 15:00:52 +0100

    Created shared module and pulled that in by refactoring

--- APM - Start/app/products/product.module.ts
+++ APM - Start/app/products/product.module.ts
@@ -1,25 +1,20 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { SharedModule } from '../shared/shared.module';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductDetailGuard } from './product-guard-service';
 import { ProductFilterPipe } from './product-filter.pipe';
 import { ProductService } from './product.service';
-import { StarComponent } from '../shared/star.component';
 
 @NgModule({
     declarations: [
         ProductListComponent,
         ProductDetailComponent,
         ProductFilterPipe,
-        StarComponent
     ],
     imports: [
-        FormsModule,
-        CommonModule,
+        SharedModule,
         RouterModule.forChild([
             { path: 'products', component: ProductListComponent },
             { path: 'product/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent },
