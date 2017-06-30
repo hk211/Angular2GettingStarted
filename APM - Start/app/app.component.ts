8a3de1878905abe85a5b9f3cf66430c24a980a77
OFFICE\hkaraagac <hakan.karaagac@netnames.com>
2017-06-30 14:16:16 +0100

    Missed files from last refactor check-in

--- APM - Start/app/app.component.ts
+++ APM - Start/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ProductService } from './products/product.service';
 
 @Component({
     selector: 'pm-app',
@@ -18,7 +17,6 @@ import { ProductService } from './products/product.service';
     <div class='container'>
         <router-outlet></router-outlet>
     </div>
-    `,
-    providers: [ProductService]
+    `
 })
 export class AppComponent { pageTitle: string = 'Acme Product Management';}
--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -1,29 +1,23 @@
 import { AppComponent }  from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { ProductListComponent } from './products/product-list.component';
-import { ProductDetailComponent } from './products/product-detail.component';
-import { ProductDetailGuard } from './products/product-guard-service';
-import { ProductFilterPipe } from './products/product-filter.pipe';
-import { StarComponent } from './shared/star.component';
+
 import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
-  imports: [BrowserModule, HttpModule, FormsModule, RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'product/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent },
+  imports: [BrowserModule, HttpModule, RouterModule.forRoot([
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full'},
       { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
-    ])
+    ]),
+    ProductModule
   ],
   declarations: [               // for components, directives and pipes that belong to this module
-    AppComponent, ProductListComponent, ProductFilterPipe, StarComponent, WelcomeComponent, ProductDetailComponent
+    AppComponent, WelcomeComponent
   ],
-  providers: [ ProductDetailGuard ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
